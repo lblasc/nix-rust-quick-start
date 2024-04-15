40d891597c2fc0fbb65f31c015ad33c3e82aa881
Luka Blaskovic <lblasc@znode.net>
2024-04-15 10:08:00 +0200

    nix-tree
    nix build --print-out-paths | xargs -o nix-tree

--- flake.nix
+++ flake.nix
@@ -142,6 +142,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
+            pkgs.nix-tree
             # pkgs.ripgrep
           ];
         };
